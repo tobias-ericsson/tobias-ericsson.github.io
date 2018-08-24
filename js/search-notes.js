c7e7c9d38a84f01118410bc7c1b4b6d1b8856c9e
Tobias Ericsson <tobias.ericsson@mobenga.com>
2018-08-24 16:20:37 +0200

    docker sysdig

--- js/search-notes.js
+++ js/search-notes.js
@@ -151,13 +151,28 @@ services:
       - /:/rootfs:ro
       - /var/run:/var/run:rw
       - /sys:/sys:ro
-      - /var/lib/docker/:/var/lib/docker:ro`,"notes/Containers/docker-compose.yml","note","2018-08-21");
+      - /var/lib/docker/:/var/lib/docker:ro
+  sysdig:
+    container_name: sysdig
+    privileged: true
+    stdin_open: true
+    tty: true
+    image: sysdig/sysdig:latest
+    volumes:
+      - /var/run/docker.sock:/host/var/run/docker.sock
+      - /dev:/host/dev
+      - /proc:/host/proc:ro
+      - /boot:/host/boot:ro
+      - /lib/modules:/host/lib/modules:ro
+      - /usr:/host/usr:ro`,"notes/Containers/docker-compose.yml","note","2018-08-21");
 search.addDoc(
 `!/usr/bin/env bash
 from  https://docs.docker.com/compose/install/
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-(uname -s)-(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version`,"notes/Containers/install-docker-compose.sh","note","2018-08-21");
+search.addDoc(
+`https://github.com/draios/sysdig/wiki/Sysdig-Examples`,"notes/Containers/sysdig.md","note","");
 search.addDoc(
 `autojump - a faster way to navigate your filesystem
 https://github.com/joelthelion/autojump
@@ -273,22 +288,31 @@ convert -resize 1024X768  source.png dest.png
 convert -resize 40% source.png dest.png
 `,"notes/NIX/images.md","note","2018-08-13");
 search.addDoc(
-`Problem booting ubuntu
+`installing
+Problem booting ubuntu
 [   3.516984] bcma: Unsupported SPROM revision: 11
 [   3.517071] bcma: bus0: No SPROM available
 * add nomodset to boot params (between quiet and splash)
 Problem installing GRUB2 (grub-efi-amd64-signed failed installation /target/)
 * you need to ensure that a partition of the ‘EFI system partition’ type is mounted at /boot/efi
 * disable secure boot in UEFI BIOS
-*** Problem with no wireless
-* dmesg shows
+*** Problem = no wireless
+dmesg shows
 Broadcom 4352 WLAN found
 ERROR: FOUND UNSUPPORTED PHY
-* sudo update && sudo apt-get install bcmwl-kernel-source
+
+sudo update && sudo apt-get install bcmwl-kernel-source
+
 *** install NVIDIA drivers
+
 ubuntu-drivers devices
 sudo ubuntu-drivers autoinstall
-`,"notes/NIX/xubuntu.md","note","2018-08-13");
+
+ Window manager shortcuts
+Alt+F7 for maximizing windows (both vertically and horizontally)
+Alt+Space for the window operations menu
+ grab and move key
+Alt + left mouse button`,"notes/NIX/xubuntu.md","note","2018-08-13");
 search.addDoc(
 `Android Debug Bridge (ADB)
 -------------------------
