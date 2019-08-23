a2baccaf82a0ce43b0bc849b4549a3b79352f2a0
Tobias Ericsson <t.ericsson@gmail.com>
2019-08-23 11:03:23 +0200

    tig

--- js/search-notes.js
+++ js/search-notes.js
@@ -78,6 +78,12 @@ git config --global credential.helper store
 
 Stores username and password in ~/.git-credentials 
 so that you don't have to provide it every time.
+ Tig
+Text-mode interface for git
+https://jonas.github.io/tig/
+sudo apt-get install tig
+add custom bindigs to .tigrc
+https://github.com/jonas/tig/wiki/Bindings
 `,"notes/Coding/git.md","note","2019-04-02");
 search.addDoc(
 ` Generate different types of For Loops iterations by typing it
