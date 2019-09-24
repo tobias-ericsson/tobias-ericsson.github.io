5d9b104d9c538770cd46191ae9e58a6b1e3c6e92
Tobias Ericsson <t.ericsson@gmail.com>
2019-09-24 12:39:33 +0200

    generate links

--- generators/generateLinks.js
+++ generators/generateLinks.js
@@ -15,21 +15,43 @@ var outFile = fs.createWriteStream(fileOutPath, {
 fs.truncateSync(fileOutPath, 0);
 
 outFile.write(generatedFileText);
+let outLines = []
+let lineReaders = 0;
 fs.readdirSync(linksFolder).forEach(function (file) {
     if (file != 'index.html') {
         console.log("reading", file);
         var lineReader = readLine.createInterface({
             input: fs.createReadStream(linksFolder + file)
         });
+        lineReaders++;
 
         lineReader.on('line', function (line) {
             if (line != '') {
-                console.log('writing', line);
-                outFile.write(preLine + line + postLine + '\n');
+                console.log('pushing', line);
+                outLines.push(preLine + line + postLine + '\n');
+            }
+        });
+
+        lineReader.on('end', function () {
+            console.log("end");
+        });
+
+        lineReader.on('close', function () {
+            console.log("close");
+            lineReaders--;
+            console.log("current lineReaders "+lineReaders);
+            if (lineReaders<1) {
+                console.log('sorting '+ outLines.length);
+                outLines.sort();
+                for (var i = 0, len = outLines.length; i < len; i++) {
+                    console.log('writing', outLines[i]);
+                    fs.writeFileSync(fileOutPath, outLines[i], {'flag': 'a'});
+                }
             }
         });
     }
 });
+
 
 
 
